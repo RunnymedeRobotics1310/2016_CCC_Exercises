a93791cb93f09e8903794720a5ef5a774421da7b
richardemcmullin <richard.e.mcmullin@gmail.com>
2015-11-01 14:47:04 -0500

    Week 1 sample problems

---
+++ src/ccc2014/S3.java
@@ -0,0 +1,140 @@
+package ccc2014;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Problem S3: The Geneva Confection
+
+Problem Description
+In order to ensure peace and prosperity for future generations, the United Nations is creating the
+world’s largest candy. The ingredients must be taken in railway cars from the top of a mountain
+and poured into Lake Geneva. The railway system goes steeply from the mountaintop down to the
+lake, with a T-shaped branch in the middle as shown below.
+Right now, each of the N ingredients is in its own railway car. Each railway car is assigned a positive
+integer from 1 to N. The ingredients must be poured into the lake in the order 1, 2, 3, . . . , N
+but the railway cars are lined up in some random order. The difficulty is that, because of the especially
+heavy gravity today, you can only move cars downhill to the lake, or sideways on the branch
+line. Is it still possible to pour the ingredients into the lake in the order 1, 2, 3, . . . , N ?
+
+For example, if the cars were in the order 2, 3, 1, 4, we can slide these into the lake in order as
+described below:
+• Slide car 4 out to the branch
+• Slide car 1 into the lake
+• Slide car 3 out to the branch
+• Slide car 2 into the lake
+• Slide car 3 from the branch into the lake
+• Slide car 4 from the branch into the lake
+
+Input Specification
+The first line will contain the number T (1 <= T <= 10) which is the number of different tests that
+will be run. Each test has the form of an integer N (1 <= N <= 100 000) on the first line of the test,
+followed by a list of the N cars listed from top to bottom. The cars will always use the numbers
+from 1 to N in some order.
+
+Output Specification
+For each test, output one line which will contain either Y (for “yum”) if the recipe can be completed,
+and N otherwise.
+ */
+/**
+ * Problem S3: The Geneva Confection
+ * 
+ * http://www.cemc.uwaterloo.ca/contests/computing/2014/stage%201/seniorEn.pdf
+ */
+public class S3 {
+
+	Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		S3 puzzle = new S3();
+		puzzle.run();
+	}
+
+	// *************************************************
+
+	// Solve the problem using decks
+	// ArrayDeque is used with Deques similarly to ArrayLists and Lists
+	private Deque<Integer> mountain;
+	private Deque<Integer> branch;
+
+	// Use an output list to buffer the output of each problem until the end.
+	private List<String> outputList;
+	
+	public void run() {
+
+		int t = new Integer(input.nextLine());
+		
+		outputList = new ArrayList<String>(t);
+
+		// For each of the puzzles, read the input and solve.
+		// Add the outputs to an output list and print them all solutions at the end.
+		for (int i = 0; i < t; i++) {
+			readSubPuzzleInput();
+			String solution = solveSubPuzzle();
+			outputList.add(solution);
+		}
+		
+		for (String solution: outputList) {
+			System.out.println(solution);
+		}
+	}
+
+	private void readSubPuzzleInput() {
+
+		int n = new Integer(input.nextLine());
+
+		mountain = new ArrayDeque<Integer>(n);
+		branch   = new ArrayDeque<Integer>(n);
+
+		for (int i = 0; i < n; i++) {
+			mountain.push(new Integer(input.nextLine()));
+		}
+	}
+
+	private String solveSubPuzzle() {
+
+		// Capture the total number of ingredients before starting to pull cars off the mountain
+		int totalIngredients = mountain.size();
+		
+		for (int ingredient = 1; ingredient <= totalIngredients; ingredient++) {
+
+			// Look to see if the ingredient is on the branch
+			if (!branch.isEmpty()) {
+				
+				// peek looks at the first ingredient without pulling 
+				// it off the branch.
+				if (branch.peek() == ingredient) {
+					branch.pop();
+					continue;
+				}
+			}
+
+			// See if it is on the mountain
+			boolean ingredientFound = false;
+			while (!mountain.isEmpty()) {
+				int i = mountain.pop();
+				if (i == ingredient) {
+					ingredientFound = true;
+					break;
+				}
+				// If it is not the right ingredient then push it onto the branch.
+				branch.push(i);
+			}
+			
+			if (ingredientFound) {
+				continue;
+			}
+
+			// If the ingredient is not found as the first element of the branch,
+			// nor on the mountain, then the recipe cannot be completed. 
+			return "N";
+		}
+		
+		// If all of the ingredients were dumped in the lake, then 
+		// the recipe was completed.
+		return "Y";
+	}
+}
--- src/ccc2015/S1.java
+++ src/ccc2015/S1.java
@@ -1,9 +1,51 @@
-package sampleProblems;
+package ccc2015;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+
+Problem S1: Zero That Out
+
+Problem Description
+Your boss has asked you to add up a sequence of positive numbers to determine how much money
+your company made last year.
+Unfortunately, your boss reads out numbers incorrectly from time to time.
+Fortunately, your boss realizes when an incorrect number is read and says “zero”, meaning “ignore
+the current last number.”
+Unfortunately, your boss can make repeated mistakes, and says “zero” for each mistake.
+For example, your boss may say “One, three, five, four, zero, zero, seven, zero, zero, six”, which
+means the total is 7 as explained in the following chart:
+
+Boss statement(s) Current numbers Explanation
+“One, three, five, four” 1, 3, 5, 4 Record the first four numbers.
+“zero, zero“ 1, 3 Ignore the last two numbers.
+“seven” 1, 3, 7 Record the number 7 at the end of our list.
+“zero, zero” 1 Ignore the last two numbers.
+“six” 1, 6 We have read all numbers, and the total is 7.
+
+At any point, your boss will have said at least as many positive numbers as “zero” statements. If
+all positive numbers have been ignored, the sum is zero.
+Write a program that reads the sequence of boss statements and computes the correct sum.
+
+Input Specification
+The first line of input contains the integer K (1 <= K <= 100 000) which is the number of integers
+(including “zero”) your boss will say. On each of the next K lines, there will either be one integer
+between 1 and 100 (inclusive), or the integer 0.
+
+Output Specification
+The output is one line, containing the integer which is the correct sum of the integers read, taking
+the “zero” statements into consideration. You can assume that the output will be an integer in the
+range 0 and 1 000 000 (inclusive).
+
+*/
+
+/**
+ * Problem S1: Zero That Out
+ * 
+ * http://www.cemc.uwaterloo.ca/contests/computing/2015/stage%201/seniorEn.pdf
+ */
 public class S1 {
 
 	Scanner input = new Scanner(System.in);
@@ -15,12 +57,13 @@ public class S1 {
 	
 	//*************************************************
 	
-	List<Integer> intLs;
+	List<Integer> intLs; 
 
 	public void run() {
 		
 		int k = new Integer(input.nextLine());
 		
+		// Create an arrayList of the right size. 
 		intLs = new ArrayList<Integer>(k);
 		
 		for (int i=0; i<k; i++) {
@@ -28,7 +71,7 @@ public class S1 {
 			int n = new Integer(input.nextLine());
 			
 			if (n == 0 ) {
-				intLs.remove(intLs.size()-1);
+				intLs.remove(intLs.size()-1);  
 			} else {
 				intLs.add(n);
 			}
